ac21b380caed41e70bdccc472965e8a6754ec457
John <olearyprofessionaldevelopment@gmail.com>
2023-12-01 14:45:51 -0500

    daily

---
+++ 7kyu/vowelcount.js
@@ -0,0 +1,15 @@
+function getCount(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
